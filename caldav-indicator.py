123796f6f7ab7b09a9d8c43591d226baa0022b29
Péter Trombitás <trombipeti@gmail.com>
2020-11-27 10:03:17 +0100

    Add detection of event start/end.

--- caldav-indicator.py
+++ caldav-indicator.py
@@ -12,6 +12,7 @@ class CalDAVIndicator(object):
         self._parse_secret_file()
         self.client = caldav.DAVClient(url = self.url, username =  self.username, password = self.password)
         self.calendar = caldav.Calendar(client = self.client, url = self.url)
+        self.current_event = None
 
     def _parse_secret_file(self):
         with open('secret.txt', 'r') as sf:
@@ -31,22 +32,29 @@ class CalDAVIndicator(object):
     def main_loop(self):
         while True:
             current_events = self.calendar.date_search(
-                start = datetime.now() - timedelta(days = 10), end = datetime.now() + timedelta(minutes = 5), expand = False
+                start = datetime.now(), end = datetime.now() + timedelta(minutes = 5), expand = False
             )
-            for te in current_events:
-                event_name = te.vobject_instance.vevent.summary.value
-                event_start = te.vobject_instance.vevent.dtstart.value
-                event_end = te.vobject_instance.vevent.dtend.value
-                participants = te.vobject_instance.vevent.contents.get('attendee')
-                if participants is not None:
-                    print(event_name, event_start, event_end)
-                    participant_names = []
-                    for p in participants:
-                        name = p.params.get('CN')[0]
-                        status = p.params.get('PARTSTAT')[0]
-                        if status == "ACCEPTED" and not "Konferenzraum" in name:
-                            participant_names.append(name)
-                    print(participant_names)
+            if len(current_events) > 0:
+                current_event = current_events[0]
+                if str(self.current_event) != str(current_event):
+                    self.current_event = current_event
+                    event_name = current_event.vobject_instance.vevent.summary.value
+                    event_start = current_event.vobject_instance.vevent.dtstart.value
+                    event_end = current_event.vobject_instance.vevent.dtend.value
+                    participants = current_event.vobject_instance.vevent.contents.get('attendee')
+                    if participants is not None:
+                        print(event_name, event_start, event_end)
+                        participant_names = []
+                        for p in participants:
+                            name = p.params.get('CN')[0]
+                            status = p.params.get('PARTSTAT')[0]
+                            if status == "ACCEPTED" and not "Konferenzraum" in name:
+                                participant_names.append(name)
+                        print(participant_names)
+            else:
+                if self.current_event:
+                    print("Ended")
+                self.current_event = None
             time.sleep(self.POLL_TIMEOUT)
 
 if __name__ == '__main__':
