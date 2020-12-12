2e10d1a5b6c9b7d1173739309c24be5c35ffe21f
Péter Trombitás <trombipeti@gmail.com>
2020-12-12 22:06:39 +0100

    Run event polling on separate thread & implement simple menu with display toggle.

--- caldav-indicator.py
+++ caldav-indicator.py
@@ -32,6 +32,7 @@ class LCDIndicator(object):
         super(LCDIndicator, self).__init__()
         self.current_event = None
         self.is_lcd_on = False
+        self.manual_lcd_status = None
         self._first_line_position = 0
         self._first_line = ""
         self._second_line_position = 0
@@ -42,21 +43,37 @@ class LCDIndicator(object):
         self._stop_display_thread = False
         self._event_lock = threading.Lock()
 
+    def force_off_lcd(self):
+        self.manual_lcd_status = False
+        self._turn_off_lcd()
+
+
+    def force_on_lcd(self):
+        self.manual_lcd_status = True
+        self._turn_on_lcd
+
+    def clear_force_lcd(self):
+        self.manual_lcd_status = None
+
     def _turn_off_lcd(self):
-        self.is_lcd_on = False
-        pass
+        if self.manual_lcd_status != True:
+            self.is_lcd_on = False
+            if has_lcd:
+                pass
 
     def _turn_on_lcd(self):
-        self.is_lcd_on = True
-        pass
+        if self.manual_lcd_status != False:
+            self.is_lcd_on = True
+            if has_lcd:
+                pass
 
     def _scroll_line(self, line, current_start):
         if len(line) <= 16:
             return (line, current_start)
         else:
-            double_line = line + " "*6 + line[0:10]
+            double_line = line + " "*6 + line
             line_part = double_line[current_start : current_start + 16]
-            if current_start == len(line):
+            if current_start == len(line) + 5:
                 current_start = 0
             else:
                 current_start += 1
@@ -142,6 +159,10 @@ class CalDAVIndicator(object):
         self.calendar = caldav.Calendar(client = self.client, url = self.url)
         self.lcd_indicator = LCDIndicator()
 
+        self._poll_events_thread_lock = threading.Lock()
+        self._poll_events_thread = threading.Thread(target = self._poll_events_loop, args = ())
+        self._stop_poll_events_thread = False
+
     def _parse_secret_file(self):
         try:
             with open('secret.txt', 'r') as sf:
@@ -179,47 +200,136 @@ class CalDAVIndicator(object):
                     'togglApiKey=' + self.togglApiKey
                 )
 
-    def is_working_toggl(self):
+    def _is_working_toggl(self):
         auth = HTTPBasicAuth(self.togglApiKey, 'api_token')
         r = requests.get("https://api.track.toggl.com/api/v8/time_entries/current", auth = auth)
         rd = json.loads(r.text)
         return rd['data'] is not None
 
-    def main_loop(self):
-        first_run = True
-        self.lcd_indicator.start_display_thread()
-        while True:
-            if self.is_working_toggl() or True:
-                current_events = self.calendar.date_search(
-                    start = datetime.now() - timedelta(days = 3), end = datetime.now(), expand = False
-                )
-                if len(current_events) > 0:
-                    current_event = current_events[0]
-                    event_name = current_event.vobject_instance.vevent.summary.value
-                    event_start = current_event.vobject_instance.vevent.dtstart.value.strftime("%-H:%M")
-                    event_end = current_event.vobject_instance.vevent.dtend.value.strftime("%-H:%M")
-                    participants = current_event.vobject_instance.vevent.contents.get('attendee')
-                    participant_names = []
-                    if participants is not None:
-                        for p in participants:
-                            name = p.params.get('CN')[0]
-                            status = p.params.get('PARTSTAT')[0]
-                            if status == "ACCEPTED" and not "Konferenzraum" in name:
-                                if ',' in name:
-                                    parts = name.split(',')
-                                    name = parts[1].strip() + ' ' + parts[0].strip()
-                                participant_names.append(name)
-                    else:
-                        print(event_name, "- without participants")
+    def stop_poll_events_thread(self):
+        with self._poll_events_thread_lock:
+            self._stop_poll_events_thread = True
+        self._poll_events_thread.join()
+
+    def _should_stop_poll_events_thread(self):
+        with self._poll_events_thread_lock:
+            return self._stop_poll_events_thread
+
+    def _poll_events_loop(self):
+        last_poll = None
+        while not self._should_stop_poll_events_thread():
+            if last_poll is None or timer() - last_poll >= self.POLL_TIMEOUT:
+                self._poll_events()
+                last_poll = timer()
+            time.sleep(0.1)
+
+
+    def _poll_events(self):
+        if self._is_working_toggl() or True:
+            current_events = self.calendar.date_search(
+                start = datetime.now() - timedelta(days = 3), end = datetime.now(), expand = False
+            )
+            if len(current_events) > 0:
+                current_event = current_events[0]
+                event_name = current_event.vobject_instance.vevent.summary.value
+                event_start = current_event.vobject_instance.vevent.dtstart.value.strftime("%-H:%M")
+                event_end = current_event.vobject_instance.vevent.dtend.value.strftime("%-H:%M")
+                participants = current_event.vobject_instance.vevent.contents.get('attendee')
+                participant_names = []
+                if participants is not None or self.DISPLAY_SINGLE_EVENTS:
+                    for p in participants:
+                        name = p.params.get('CN')[0]
+                        status = p.params.get('PARTSTAT')[0]
+                        if status == "ACCEPTED" and not "Konferenzraum" in name and not "Trombitas" in name:
+                            if ',' in name:
+                                parts = name.split(',')
+                                name = parts[1].strip() + ' ' + parts[0].strip()
+                            participant_names.append(name)
                     display_event = CalendarDisplayEvent(event_name, event_start, event_end, participant_names)
                     self.lcd_indicator.set_current_event(display_event)
-                else:
-                    self.lcd_indicator.set_current_event(None)
             else:
-                print("Not working")
                 self.lcd_indicator.set_current_event(None)
-            time.sleep(self.POLL_TIMEOUT)
-            first_run = False
+        else:
+            self.lcd_indicator.set_current_event(None)
+
+    def _config_menu(self):
+        print(
+            'Configuration\n'
+            ' p: poll timeout\n'
+            ' d: display frequency'
+        )
+        choice = input('> ')
+        if choice == 'p':
+            try:
+                self.POLL_TIMEOUT = float(input('Poll timeout ({0}): '.format(self.POLL_TIMEOUT)))
+            except ValueError:
+                pass
+        elif choice == 'd':
+            try:
+                prompt = 'Display update frequency ({0}): '.format(self.lcd_indicator.DISPLAY_UPDATE_FREQ)
+                self.lcd_indicator.DISPLAY_UPDATE_FREQ = float(input(prompt))
+            except ValueError:
+                pass
+
+    def _manual_input_menu(self):
+        name = input("Event name: ")
+        if len(name.strip()) > 0:
+            start = datetime.now().strftime("%-H:%M")
+            end = input("End: ")
+            participants = []
+            print("Participants:")
+            while True:
+                p = input("> ")
+                if len(p.strip()) == 0:
+                    break
+                participants.append(p)
+            self.lcd_indicator.set_current_event(CalendarDisplayEvent(name, start, end, participants))
+
+    def _display_menu(self):
+        print(
+            'Display\n'
+            ' on\n'
+            ' off\n'
+            ' clear'
+        )
+        choice = input('> ')
+        if choice == 'on':
+            self.lcd_indicator.force_on_lcd()
+        elif choice == 'off':
+            self.lcd_indicator.force_off_lcd()
+        elif choice == 'clear':
+            self.lcd_indicator.clear_force_lcd()
+
+    def _handle_user_input(self):
+        print(
+            'RPi CalDAV Indicator\n'
+            ' c: configure\n'
+            ' m: manual event input\n'
+            ' d: display menu\n'
+            ' q: quit'
+        )
+        choice = input('> ')
+        app_running = True
+        if choice == 'c':
+            self._config_menu()
+        elif choice == 'm':
+            self._manual_input_menu()
+        elif choice == 'd':
+            self._display_menu()
+        elif choice == 'q':
+            app_running = False
+        return app_running
+
+    def main_loop(self):
+        self.lcd_indicator.start_display_thread()
+        self._poll_events_thread.start()
+
+        while self._handle_user_input():
+            pass
+
+        self.lcd_indicator.stop_display_thread()
+        self.stop_poll_events_thread()
+
 
 if __name__ == '__main__':
     CalDAVIndicator().main_loop()
